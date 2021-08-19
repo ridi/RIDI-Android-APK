32cfc1948918fe2bf482712a2260a9897815ff2a
davin.ahn <m.davinahn@gmail.com>
2021-08-19 17:12:15 +0900

    Fix runtime error in IE

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+"use strict";
+
 function initialize() {
-  const url = 'https://viewer-ota.ridicdn.net/android';
-  const current = '21.7.4';
-  const available = [
+  var url = 'https://viewer-ota.ridicdn.net/android';
+  var current = '21.7.4';
+  var available = [
     'Release Notes.txt',
     'Ridibooks-20.1.2-E-Ink-noTTS.apk',
     'Ridibooks-20.1.2-E-Ink.apk',
@@ -9,17 +11,17 @@ function initialize() {
     'Ridibooks-20.1.2-PlayStore.apk',
     'Ridibooks-20.5.1-E-Ink-noTTS.apk',
     'Ridibooks-20.5.1-E-Ink.apk',
-    `Ridibooks-${current}-ARMv7aOnly.apk`,
-    `Ridibooks-${current}-PlayStore.apk`,
+    'Ridibooks-' + current + '-ARMv7aOnly.apk',
+    'Ridibooks-' + current + '-PlayStore.apk',
   ];
 
-  const container = document.getElementById('container');
-  available.forEach(path => {
-    const item = document.createElement('div');
+  var container = document.getElementById('container');
+  available.forEach(function (path) {
+    var item = document.createElement('div');
     item.classList.add('item');
     item.innerText = path;
-    item.onclick = () => {
-      location.href = `${url}/${encodeURIComponent(path)}`;
+    item.onclick = function () {
+      location.href = url + '/' + encodeURIComponent(path);
     };
     container.appendChild(item);
   });

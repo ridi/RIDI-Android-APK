62da80ff0c51aba9d0dfdf1327cee107798acb19
Davin Ahn <davin.ahn@ridi.com>
2021-12-29 18:02:09 +0900

    Bump version to 21.10.3

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '21.10.2';
+  var current = '21.10.3';
   var available = [
     'Release Notes.txt',
     'Ridibooks-20.1.2-E-Ink-noTTS.apk',

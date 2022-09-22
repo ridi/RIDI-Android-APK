9491d6755c9325b236f202f979b869658864175c
Davin Ahn <davin.ahn@ridi.com>
2022-09-22 13:03:04 +0900

    Bump version to 22.9.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '22.8.3';
+  var current = '22.9.1';
   var available = [
     'Release Notes.txt',
     'RIDI-20.1.2-E-Ink-noTTS.apk',

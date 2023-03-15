0daecf98c3d6b7d545ada04c274f620478b8946e
Davin Ahn <davin.ahn@ridi.com>
2023-03-15 19:39:14 +0900

    Bump version to 23.3.2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '23.3.1';
+  var current = '23.3.2';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

8a5eb4bb0190f3796af9af4ce1de03546a39a8ef
DeokHyeon Wi <dh.wi@ridi.com>
2023-06-13 18:45:26 +0900

    Bump version to 23.5.2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '23.5.1';
+  var current = '23.5.2';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

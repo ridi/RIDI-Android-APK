7372efd4e14522fb2a14cfeaca08fab301b24bfe
김경태 <kt.kim@ridi.com>
2023-11-09 17:40:11 +0900

    bump version to 23.10.2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '23.10.1';
+  var current = '23.10.2';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

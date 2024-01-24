3f9ee8fe58d123b5094420ea55fcecf9d76baf5f
Davin Ahn <davin.ahn@ridi.com>
2024-01-24 10:49:44 +0900

    Bump version to 24.1.2

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '24.1.1';
+  var current = '24.1.2';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

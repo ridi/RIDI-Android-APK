bd8a68f63754720bb2b288387cc7e3733cb8d15c
김경태 <kt.kim@ridi.com>
2024-03-25 10:58:05 +0900

    Bump version to 24.3.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '24.1.4';
+  var current = '24.3.1';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

82d9b1c714101dcd86b3dc42514d8ed04f05ae49
jiggag <jiggag90@gmail.com>
2025-04-02 11:07:13 +0900

    Bump version to 25.4.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '25.3.1';
+  var current = '25.4.1';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

269a0c9eb27b8a1ea5923e250c0eae4b28d0e84c
DeokHyeon Wi <dh.wi@ridi.com>
2025-08-18 14:54:46 +0900

    Bump version to 25.8.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '25.7.2';
+  var current = '25.8.1';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

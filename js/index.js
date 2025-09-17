262b0ac29289c07d38480859073a8570117787db
jiggag <jiggag90@gmail.com>
2025-09-17 17:11:57 +0900

    Bump version to 25.9.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '25.8.2';
+  var current = '25.9.1';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

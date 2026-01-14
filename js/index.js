9bc4e47c09d968855ac26a42b805fc6559397a8a
jiggag <jiggag90@gmail.com>
2026-01-14 15:30:11 +0900

    Bump version to 26.1.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '25.12.2';
+  var current = '26.1.1';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',

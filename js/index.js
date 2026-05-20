2971475d3eadcf86e0fc0087d9dc5c3d06f4d168
Yooseok Yoon <YSYoon@users.noreply.github.com>
2026-05-20 12:45:36 +0900

    Bump version to 26.5.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function initialize() {
   var url = 'https://viewer-ota.ridicdn.net/android';
-  var current = '26.4.2';
+  var current = '26.5.1';
   var available = [
     'Release Notes.txt',
     'RIDI-' + current + '-OneStore-ARMv7aOnly.apk',
